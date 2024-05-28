d7ce38ed8546e580408d7abc031ad8a3457fcce5
lnit <6626484+lnit@users.noreply.github.com>
2024-05-28 19:36:28 +0900

    IndexedDB経由でOASを保存できるように

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,5 @@
 import { DefaultRubyVM } from "https://cdn.jsdelivr.net/npm/@ruby/wasm-wasi@2.6.1/dist/browser/+esm";
+import { openDB, deleteDB, wrap, unwrap  } from 'https://cdn.jsdelivr.net/npm/idb@8/+esm';
 
 const initRuby = async () => {
   const response = await fetch("/my-ruby-app.wasm");
@@ -18,12 +19,17 @@ const initRuby = async () => {
 
 self.addEventListener('fetch', (event) => {
   const bootResources = [
-    "/boot.html", "/service-worker.js"
+    "/boot.html", "/service-worker.js", "/my-ruby-app.wasm"
   ]
   if (bootResources.find((r) => event.request.url.endsWith(r))) {
-      console.log('[rails-web] Fetching boot files from network:', event.request.url);
-      event.respondWith(fetch(event.request.url));
-      return;
+    console.log('[Service Worker] Fetching boot files from network:', event.request.url);
+    event.respondWith(fetch(event.request.url));
+    return;
+  }
+  if (event.request.url.includes("cdn.jsdelivr.net")) {
+    console.log('[Service Worker] Fetching lib files from network:', event.request.url);
+    event.respondWith(fetch(event.request.url));
+    return;
   }
 
   event.respondWith(
@@ -38,8 +44,15 @@ const respondWithRuby = async (event) => {
     vm = self.RubyVM;
   }
 
+  const specs = await loadSpecsFromDB();
+  console.log("[Service Worker] Specs loaded from DB");
+
   const command = `
-    mock = MockServer.new(File.open("src/sample.yml"))
+    specs_str = <<EOS
+${specs.specs}
+EOS
+
+    mock = MockServer.new(specs_str)
     mock.response(method: '${event.request.method}', url: '${event.request.url}')
   `
   const res = vm.eval(command).toJS();
@@ -49,7 +62,10 @@ const respondWithRuby = async (event) => {
   return new Response(body, { headers, status: status });
 }
 
-const fetchOASYaml = async () => {
-  const response = await fetch("/sample.yml");
-  return await response.text();
+const loadSpecsFromDB = async () => {
+  let db = await openDB("OpenApiDatabase", 1, db => {});
+
+  let transaction = db.transaction('apiSpecs', 'readwrite');
+  let apiSpecs = transaction.objectStore('apiSpecs');
+  return apiSpecs.get(1)
 }
