40aea36c90b8975cf8d9605869d257676e046f84
lnit <6626484+lnit@users.noreply.github.com>
2024-06-13 01:06:51 +0900

    OpenAPI仕様にexample blockが定義されていない場合にレスポンスを自動生成する機能の下地を作成
    - openapi3_parserに機能追加する形で実装

---
+++ src/patch.rb
@@ -0,0 +1,43 @@
+require "openapi3_parser"
+
+module Openapi3Parser
+  module Node
+    class MediaType < Node::Object
+      def example_value
+        return example if example
+        return schema.example if schema.example
+
+        schema.example_value
+      end
+    end
+
+    class Schema < Node::Object
+      def example_value
+        return example if example
+
+        case type
+        when "array"
+          return [items.example_value]
+        when "object"
+          return properties.example_value
+        when "number", "integer"
+          return 1
+        when "string"
+          return "string"
+        else
+          return "test!"
+        end
+      end
+    end
+
+    class Map
+      def example_value
+        keys.map do |key|
+          [key, self[key].example_value]
+        end.to_h
+      end
+    end
+  end
+end
+
+puts "load completed"
